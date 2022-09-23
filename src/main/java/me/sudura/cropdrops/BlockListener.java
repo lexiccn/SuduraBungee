1bcc137e766c0c77bff24e22e954f65fce82acd4
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 15:07:21 +0100

    Rewritten to use a config file!

--- src/main/java/me/sudura/cropdrops/BlockListener.java
+++ src/main/java/me/sudura/cropdrops/BlockListener.java
@@ -7,17 +7,24 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BlockListener implements Listener {
     CropDrops plugin;
+    HashMap<Material, Integer> xpMaterials = new HashMap<>();
     public BlockListener(CropDrops instance) {
         plugin = instance;
+        for (Map.Entry<String, Object> mat : instance.getConfig().getConfigurationSection("xp").getValues(false).entrySet()) {
+            xpMaterials.put(Material.getMaterial(mat.getKey()), (Integer)mat.getValue());
+        }
     }
 
     @EventHandler (priority = EventPriority.LOWEST)
     public void onBreakBlock (BlockBreakEvent event) {
         BlockState blockState = event.getBlock().getState();
-        if ((blockState.getType() == Material.MELON || blockState.getType() == Material.PUMPKIN) && !plugin.mmo.getPlaceStore().isTrue(blockState)) {
-            event.setExpToDrop(20);
+        if ((xpMaterials.containsKey(blockState.getType())) && !plugin.mmo.getPlaceStore().isTrue(blockState)){
+            event.setExpToDrop(xpMaterials.get(blockState.getType()));
         }
     }
 }
--- src/main/java/me/sudura/cropdrops/CropDrops.java
+++ src/main/java/me/sudura/cropdrops/CropDrops.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CropDrops extends JavaPlugin {
     mcMMO mmo;
     public void onEnable() {
+        this.saveDefaultConfig();
         this.getServer().getPluginManager().registerEvents(new BlockListener(this), this);
         if (this.getServer().getPluginManager().isPluginEnabled("mcMMO")) {
             mmo = (mcMMO) this.getServer().getPluginManager().getPlugin("mcMMO");
